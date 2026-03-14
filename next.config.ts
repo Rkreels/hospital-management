9e86dd2f4238702b1732da16904729343dc0a925
mycampushub <mycampuslifestyle@gmail.com>
2026-03-14 13:15:54 +0600

    vercel json updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  basePath: "/hospital-management",
+  assetPrefix: "/hospital-management",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
